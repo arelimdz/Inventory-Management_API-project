9a379f546942364a82782bcd2746b7fbf2a8d2ee
arelimdz <arelimendozaperez@gmail.com>
2023-07-30 22:59:33 +1000

    Fix casing

--- src/controllers/auth_controller.py
+++ src/controllers/auth_controller.py
@@ -52,7 +52,7 @@ def auth_login():
         token = create_access_token(
             identity=str(user.id), expires_delta=(timedelta(days=1))
         )
-        return {"email": user.email, "token": token, "is_admin": user.is_admin}
+        return {"email": user.email, "token": token, "isAdmin": user.is_admin}
     else:
         return {"error": "Invalid email or password"}, 401
 
